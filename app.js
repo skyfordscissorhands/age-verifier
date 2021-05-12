5219a930d38f573c3665bb19ee597548592d6030
skynewell1 <skyfordnewell@gmail.com>
2021-05-11 21:30:57 -0600

    push

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ var age = prompt("How old are you?");
 if (age >= 18) alert('Hi!')
 else {
     alert('You get to watch a puppy video!')
-    document.location = "https://www.youtube.com/watch?v=DUfKZjRo8GY";
+    document.location.href = "https://www.youtube.com/watch?v=DUfKZjRo8GY";
 }
 
 
