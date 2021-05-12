d8f58e3b804383ac292a1cf484963d9db11c030c
skynewell1 <skyfordnewell@gmail.com>
2021-05-11 21:25:33 -0600

    push

---
+++ app.js
@@ -0,0 +1,160 @@
+// function timeOfDay() {
+//     var hour = new Date().getHours();
+//     var greeting;
+// if (hour < 18) { greeting = "Good day";}
+// else{"Good evening"} alert(greeting);}
+// timeOfDay(true);
+
+
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+// document.write("Hello World"+'<br />');
+
+// for(let i = 0; i < 10; i++){
+//     document.write('Hello World' + '<br/v');
+// }
+
+// for(i=0; i<10; i++){
+//     if (i===7){
+//       break;
+//     }
+
+// document.write("the number is"+ i +"<br>");
+
+// / while loop
+// var x=0;
+// while(x<5) {
+//     console.log(x);
+//     x = x+1;
+// };
+
+// var x = 5
+// while (x<5){
+//     console.log(x);
+//     x = x +1;
+// };
+
+
+
+// for (var i=0 ; i <20; i++){
+//     if(1 % 2===0){
+//         document.write(i + ' is even ')
+//     } else {
+//         document.write(i + " is odd ");
+//     }
+// }
+
+// var money = 1500.00;
+// var bills = 2500.00;
+// if (money > bills || money ===bills){
+//     document.write('You are covered!')
+// }
+// else if (money === bills || money < 2000){
+//     document.write('you are good, just barely')
+// }
+// else { document.write('uh-oh!')
+// };
+
+// function assignGrade(score){
+//     if (score>90){
+//         return 'A';
+//     }else if (score > 80){
+//         return 'B';
+//     }else if (score> 70) {
+//         return 'C';
+//     }else if (score> 60) {
+//         return 'C';
+//     }else if (score> 50) {
+//         return 'D';
+//     }else if (score> 40) {
+//         return 'F';
+//     }
+
+// }
+// 
+// var score = 65;
+
+// switch(score){
+//     case (score === 100);
+//     console.log('A');
+//     break;
+
+//     case (score > 80);
+//     console.log('B');
+//     break;
+
+//     case (score < 65);
+//     console.log('A');
+//     break;
+
+//     default;
+//     console.log('You Failed');
+//     break;
+
+// }
+
+
+// document.write("Your grade is" + assignGrade(78));
+// document.write("Your grade is" + assignGrade(92));
+// document.write("Your grade is" + assignGrade(51));
+var age = prompt("How old are you?");
+if (age >= 18) alert('Hi!')
+else {
+    alert('You get to watch a puppy video!')
+    document.location = "https://www.youtube.com/watch?v=DUfKZjRo8GY";
+}
+
+
+var score = 100;
+
+switch(true){
+    case(score > 90):
+    console.log("You got an A!");
+    break;
+
+    case(score > 80):
+    console.log("You got a B.");
+    break;
+
+    case(score > 70):
+    console.log("You got an C");
+    break;
+
+    case(score > 60):
+    console.log("You got an D");
+    break;
+
+    default:
+    console.log("You failed");
+}
+    
+// }
+// String letterGrade;
+// switch (score) 
+//     {
+//     case A(score > 90):
+// document.write ("You got an A.")
+//       break;
+//     case B (score > 80):
+// document.write ("You got a B.")
+//       break;
+//     case C (score > 70):
+// document.write ("You got a C.")
+//       break;
+//     case D (score > 60):
+// document.write ("You got a D.")
+//       break;
+//     default:
+//         document.write("You failed");
+//     }
+ 
+//  return letterGrade;
+// };
