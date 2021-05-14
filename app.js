f3a548e9307159aff2a3f76d254f1d1c208cdef6
skynewell1 <skyfordnewell@gmail.com>
2021-05-13 21:14:08 -0600

    push

--- app.js
+++ app.js
@@ -106,7 +106,7 @@
 // document.write("Your grade is" + assignGrade(92));
 // document.write("Your grade is" + assignGrade(51));
 var age = prompt("How old are you?");
-if (age >= 18) alert('Hi!')
+if (age >= 18) alert('You are old!')
 else {
     alert('You get to watch a puppy video!')
     document.location.href = "https://www.youtube.com/watch?v=DUfKZjRo8GY";
@@ -158,3 +158,98 @@ switch(true){
  
 //  return letterGrade;
 // };
+
+
+var day; 
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+     day = "Tuesday";
+      break;
+    case 3:
+      day= "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day= "Saturday";
+      default: "no day" ;
+  }
+  alert (day);
+
+//   object literal store the object in a variable
+var biography = {
+    name: "April",
+    age: 30,
+    likes: ["rest", "money"],
+    kids: true
+}
+// object constructor new object keyword
+
+var bio = new Object();
+    bio.name= "April";
+    bio.age= 30;
+    bio.likes= ["rest","food"]
+    bio.kids= true;
+
+    // redccipe card
+var recipe = {
+    title: 'Mole',
+    serving: 2,
+    ingredients: ["cumin","chocolate","milk"]
+};
+console.log(recipe.title);
+console.log(`serving` + recipe.servings);
+console.log(`ingredients:`);
+for (var i = 0; i < recipe.ingredients.length; i++) {
+    console.log(recipe.ingredients[i]);
+}
+
+// Keep track of which books you read and which books you want to read!
+// Create an array of objects, where each object describes a book and has properties for
+//  the title (a string), author (a string), and alreadyRead (a boolean indicating if you
+//  read it yet).
+// Iterate through the array of books. For each book, log the book title and book author 
+// like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet 
+// or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. 
+// Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" 
+// by J.R.R. Tolkien.'
+
+var books = [
+{
+    title:'The Hobbit',
+    author: 'J.R.R. Martin',
+    alreadyread: true
+},
+{
+    title: "Becoming Me" ,
+    author: "Michelle Obama",
+    alreadyread: true
+},
+{
+    title:'Daisy Jones & the six',
+    author: 'Taylor Reid',
+    alreadyread: true
+
+}];
+
+for (var i = 0; i < books.length; i++) {
+var book= books[i];
+var bookInfo= book.title + " by " + book.author;
+if (book.alreadyread){
+    console.log (`You already read ${bookInfo}`)
+} else {
+        console.log(`You still need to read ${bookInfo}`);
+    }
+}
